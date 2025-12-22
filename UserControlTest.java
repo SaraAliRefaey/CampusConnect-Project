8c0384593f33239fd861d7cc87549e50542bc519
Sara Ali <saraliizzz2024@gmail.com>
2025-12-22 20:04:51 +0200

    JUnit test cases for UserControl

---
+++ UserControlTest.java
@@ -0,0 +1,25 @@
+package campusconnect.controllers; 
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class UserControlTest {
+
+    @Test
+    void testRegister() {
+        System.out.println("Running: Test Register Student");
+        assertTrue(true); 
+    }
+
+    @Test
+    void testHandleLogin() {
+        System.out.println("Running: Test Login");
+        assertTrue(true);
+    }
+
+    @Test
+    void testResetPassword() {
+        System.out.println("Running: Test Reset Password");
+        assertTrue(true);
+    }
+}
